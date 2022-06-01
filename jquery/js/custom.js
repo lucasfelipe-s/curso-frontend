c101f6b28e17ba55edeac26a4f51b586a0df781e
Lucas Felipe <contatolucasfelipe.s@gmail.com>
2022-06-01 17:18:37 -0300

    feat: adicionar template de e-commerce EXERCICIO M17

---
+++ jquery/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
+    let titulos = $('h4') // tag
+   
+    let itens = $('.featured-item') // class
+    
+    let destaques = $('#featured') // id
+
+    console.log(titulos.first());
+
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+     
+     $('.featured-item h4').dblclick( function(){
+
+        $(this).css({
+            'color': '#f00',
+            'background': '#ff0',
+            'font-weight': '100',
+        });
+
+     });
+
+     $('.featured-item a').on('blur', function(event){
+
+        event.preventDefault();
+
+        alert('Produto esgotado');
+
+     })
+     
+})
